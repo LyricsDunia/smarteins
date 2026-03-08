87bacf32ba3715650eec26389ce4ed0084b4c478
LyricsDunia <lyricsdunia9@gmail.com>
2026-03-08 19:03:03 +0000

    Fix base path and rebuild

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/smarteins/',
   server: {
     fs: {
       allow: ['..'] // Allows Vite to access folders outside of /src
     }
   }
 })
+
