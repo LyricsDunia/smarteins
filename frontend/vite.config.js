5c964b59cede82e18eb2292792dd329fdcd6ec2c
LyricsDunia <lyricsdunia9@gmail.com>
2026-03-08 19:20:46 +0000

    Move build output to root

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/smarteins/',
+  base: '/smarteins/frontend',
   server: {
     fs: {
       allow: ['..'] // Allows Vite to access folders outside of /src
